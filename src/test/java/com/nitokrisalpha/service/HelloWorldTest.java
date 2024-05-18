6e7adf0fbec679487d8ce11ef572591ff659581a
nitokrisalpha <nitokrisalpha@outlook.com>
2024-05-18 15:54:49 +0800

    Test

---
+++ src/test/java/com/nitokrisalpha/service/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.nitokrisalpha.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloWorldTest {
+
+    @Test
+    public void test(){
+        System.out.println("hello");
+    }
+
+}
