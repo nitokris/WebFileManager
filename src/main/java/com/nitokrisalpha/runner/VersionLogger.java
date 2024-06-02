3abd444f3f11bcaa38f7c5aa643b073f9e003772
nitokrisalpha <nitokrisalpha@outlook.com>
2024-06-02 14:27:37 +0800

    add version tip

---
+++ src/main/java/com/nitokrisalpha/runner/VersionLogger.java
@@ -0,0 +1,17 @@
+package com.nitokrisalpha.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class VersionLogger implements CommandLineRunner {
+
+    private static final String version = "1.11";
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.warn(version);
+    }
+}
